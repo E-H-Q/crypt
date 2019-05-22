beb09c2211b56ea61a3ae194d9a3edeef9c2789e
Benjamin Zacharias <benzach0421@gmail.com>
2019-05-21 19:32:36 -0600

    Added "main.php": The real meat of the server.

---
+++ main.php
@@ -0,0 +1,58 @@
+<?php
+echo '
+<head>
+	<title>THE CRYPT</title>
+	<link rel="stylesheet" type="text/css" href="php.css">
+</head>
+';
+
+function main() {
+	$dir = "";
+	$dir = (isset($_GET['dir']) ? $_GET['dir'] : null);
+	if ($dir == "") {
+		$dir = "./";
+	}
+	$arr = array();
+	$i = 0;
+	$olddir = dirname(getcwd());
+	chdir($dir);
+	echo "<div class='dir'>Current dir: " . getcwd() . "</div><br>";
+	if ($olddir != "./") {
+		$olddir = "./";
+	}
+	echo "<a class='link' href ='upload.php?dir=" . $olddir . "'>" . 'RETURN' . "</a>";
+	if (isset($_GET['dir']) ? $_GET['dir'] : null) {
+		$dir = (isset($_GET['dir']) ? $_GET['dir'] : null);
+	}
+	echo "<hr>";
+	foreach (glob("*.mp4", GLOB_BRACE) as $files) {
+		array_push($arr, $files);
+		$name= preg_replace('/\\.[^.\\s]{3,4}$/', '', $arr[$i]);
+		echo "
+		<div class='container'>
+			<div class='content'>
+				<a href='$dir/$arr[$i]' target='blank' alt='$name' title='$name'>
+				<img src='$dir/$arr[$i].jpg'>
+				$name
+				</a>
+			</div>
+		</div>";
+		$i++;
+	}
+	foreach (glob("*", GLOB_ONLYDIR) as $folders) {
+		echo "
+		<div class='container'>
+			<div class='content'>
+				<a href='upload.php?dir=" . basename($dir) . "/" . $folders . "'><img src='folder.png' style='width: 85%;'>" . $folders . "
+				<i> [DIR]</i>	
+				</a>
+			</div>
+		</div>";
+		if (isset($_GET['dir']) ? $_GET['dir'] : null) {
+			$dir = (isset($_GET['dir']) ? $_GET['dir'] : null);
+		}
+	}
+}
+
+main();
+?>
